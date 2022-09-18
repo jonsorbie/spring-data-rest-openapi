f90c18942e0c56a03e589c3341f150c0786de543
Jon Sorbie <jon.sorbie@gmail.com>
2022-09-18 16:24:32 -0500

    RestExposureConfig

---
+++ src/main/java/sorbie/jon/springdatarestopenapi/config/RestExposureConfig.java
@@ -0,0 +1,21 @@
+package sorbie.jon.springdatarestopenapi.config;
+
+import org.springframework.data.rest.core.config.*;
+import org.springframework.data.rest.webmvc.config.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+import static org.springframework.http.HttpMethod.*;
+
+/**
+ * This class disables Http verbs globally across all entities.
+ */
+@Component
+public class RestExposureConfig implements RepositoryRestConfigurer {
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration restConfig, CorsRegistry cors) {
+        restConfig.getExposureConfiguration()
+            .withItemExposure((metadata, httpMethods) -> httpMethods.disable(DELETE, PATCH, PUT))
+            .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(DELETE, PATCH, PUT));
+    }
+}
