5458969b3c807f8325c8628d0de94fa79a1e350a
Jon Sorbie <jon.sorbie@gmail.com>
2022-09-18 13:04:40 -0500

    Spring initializr files

---
+++ src/main/java/sorbie/jon/springdatarestopenapi/SpringDataRestOpenapiApplication.java
@@ -0,0 +1,13 @@
+package sorbie.jon.springdatarestopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestOpenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestOpenapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sorbie/jon/springdatarestopenapi/SpringDataRestOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sorbie.jon.springdatarestopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
